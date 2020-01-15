579ba92fb360622dcb5026668bce2ca41f869045
Shannon <shannon.goemaere@student.howest.be>
2020-01-15 22:07:36 +0100

    Javascript remove step

--- script/addComponent.js
+++ script/addComponent.js
@@ -1,18 +1,24 @@
-let form, buttonAddStep, buttonAddStepBetween, inputStep, inputStepDescription;
+let form, buttonAddStep, removeButton, buttonAddStepBetween, inputStep, inputStepDescription;
 
 const getFormElements = function() {
   form = document.querySelector('.js-form-addStep');
   form.noValidate = true; //input is not validated when submitted
   // form.addEventListener('submit', onFormSubmit);
   buttonAddStep = document.querySelector('.js-button-addStep');
-  buttonAddStep.addEventListener('click', onHandlerClickAdd);
+  buttonAddStep.addEventListener('click', onHandlerClickedAdd);
   inputStep = document.querySelector('.js-single-step');
   numberStep = document.querySelector('.js-step-number');
   allSteps = document.querySelector('.js-all-steps');
   inputStepDescription = document.querySelector('.js-input-description');
 };
 
-const onHandlerClickAdd = function(e) {
+const onHandlerClickedRemove = function(e) {
+  e.preventDefault();
+  console.log(allSteps);
+  document.querySelector('.js-all-steps').removeChild3(e.currentTarget.parentNode.parentNode.parentNode.parentNode);
+};
+
+const onHandlerClickedAdd = function(e) {
   e.preventDefault();
   let inpStep = inputStep.cloneNode(true);
   let count = document.getElementsByName('stepDescription');
@@ -20,6 +26,10 @@ const onHandlerClickAdd = function(e) {
   inpStep.querySelector('.js-step-number').innerHTML = inpStep.dataset.number;
   inpStep.querySelector('.js-input-description').value = '';
   allSteps.appendChild(inpStep);
+  removeButton = document.querySelectorAll('.js-remove-button');
+  removeButton.forEach(element => {
+    element.addEventListener('click', onHandlerClickedRemove);
+  });
 };
 
 document.addEventListener('DOMContentLoaded', function() {
