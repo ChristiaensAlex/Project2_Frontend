0121609433bbcb4905ec1822d91580ab3a3fa6dd
Alexandra <alexandra.christiaens@student.howest.be>
2020-01-18 14:42:54 +0100

    Added Javascript to connect backend and frontend to register Mentor.

---
+++ script/registerMentor.js
@@ -0,0 +1,25 @@
+const url = 'https://localhost:44374/api/AuthMentor/Register';
+const postAPI = function(payload) {
+	console.log('post');
+	let body = JSON.stringify(payload);
+	console.log(body);
+	fetch(url, {
+		method: 'POST',
+		mode: 'cors',
+		cache: 'no-cache',
+		credentials: 'same-origin',
+		headers: { 'Content-Type': 'application/json' },
+		body: body
+	})
+		.then(res => res.json())
+		.then(data => console.log(data))
+		.catch(err => console.log(err));
+};
+
+const OnHandlerClickedLogin = function(payload) {
+	console.log(payload);
+	console.log('Handler');
+	// console.log(emailLogin.value);
+	// console.log(passwordLogin.value);
+	postAPI(payload);
+};
