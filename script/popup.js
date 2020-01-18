230e06eccff183f2eb45ecae16f05ce9d136f0c6
Shannon <shannon.goemaere@student.howest.be>
2020-01-18 14:47:06 +0100

    Dynamically adding clients (GET)
    Deleted designed clients in html so it is empty when there are no clients yet

--- script/popup.js
+++ script/popup.js
@@ -1,4 +1,4 @@
-let deleteButton, background;
+let deleteClientButton, background;
 
 const OnHandlerClickedCancel = function() {
   background.classList.remove('c-popup-blur');
@@ -13,15 +13,18 @@ const onHandlerClickedPopUp = function() {
 };
 
 const getElements = function() {
-  deleteButton = document.querySelector('.js-client-delete');
-  deleteButton.addEventListener('click', onHandlerClickedPopUp);
+  deleteClientButton = document.querySelectorAll('.js-client-delete');
   background = document.querySelector('.js-background-popup');
   popup = document.querySelector('.c-popup-form');
   cancelButton = document.querySelector('.js-cancel');
   cancelButton.addEventListener('click', OnHandlerClickedCancel);
-};
+  if(deleteClientButton){
+    deleteClientButton.forEach(element => {
+      element.addEventListener('click', onHandlerClickedPopUp);
+    });
+  }
+  };
 
 document.addEventListener('DOMContentLoaded', function() {
   console.log('DOM loaded');
-  getElements();
 });
