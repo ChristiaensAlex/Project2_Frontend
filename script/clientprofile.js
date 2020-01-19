e80269555ceb34c044736620975ad29eb6b8ec96
Alexandra <alexandra.christiaens@student.howest.be>
2020-01-19 12:15:36 +0100

    added function so mentor can edit  profile info client

--- script/clientprofile.js
+++ script/clientprofile.js
@@ -1,18 +1,44 @@
+let firstname, lastname, username, password, extra, profilepic;
+const isEmpty = function(fieldValue) {
+	return !fieldValue || !fieldValue.length;
+};
+const putClientInfoAPI = function(url, payload) {
+	console.log('put client info');
+	let body = JSON.stringify(payload);
+	console.log(body);
+	fetch(url, {
+		method: 'PUT',
+		mode: 'cors',
+		cache: 'no-cache',
+		credentials: 'same-origin',
+		headers: { 'Content-Type': 'application/json' },
+		body: body
+	})
+		.then(res => {
+			console.log(res.status)((window.location.href = 'MentorHasClientList.html'));
+		})
+		// .then(data => {
+		// 	console.log(data); // ;
+		// })
+		.catch(err => console.log(err));
+};
+
 const showClientInfo = function(json) {
-    firstname.value = json.firstName;
-    lastname.value = json.lastName;
-    username.value = json.username;
-    password.value = json.password;
-    extra.value = json.infoClient;
+	firstname.value = json.firstName;
+	lastname.value = json.lastName;
+	username.value = json.username;
+	password.value = json.password;
+	extra.value = json.infoClient;
 };
 
 const GetDomElements = function() {
-    firstname = document.querySelector('.js-firstnameClient');
-    lastname = document.querySelector('.js-lastnameClient');
-    username = document.querySelector('.js-username');
-    password = document.querySelector('.js-password');
-    extra=document.querySelector('.js-extraClientInfo');
-
+	firstname = document.querySelector('.js-firstnameClient');
+	lastname = document.querySelector('.js-lastnameClient');
+	username = document.querySelector('.js-username');
+	password = document.querySelector('.js-password');
+	extra = document.querySelector('.js-extraClientInfo');
+	updateButton = document.querySelector('.js-submitbutton');
+	ListenToUpdateButton(updateButton);
 };
 
 const getAPI = function(url) {
@@ -34,9 +60,27 @@ const getAPI = function(url) {
 		});
 };
 
+const ListenToUpdateButton = function(button) {
+	button.addEventListener('click', function(event) {
+		event.preventDefault();
+		if (!isEmpty(firstname.value) && !isEmpty(password.value)) {
+			let payload = {
+				firstName: firstname.value,
+				lastName: lastname.value,
+				username: username.value,
+				password: password.value,
+				confirmPassword: password.value,
+				infoClient: extra.value,
+				profilePictureId: null
+			};
+			putClientInfoAPI(`https://localhost:44374/api/client/${sessionStorage.clientId}`, payload);
+		}
+	});
+};
+
 document.addEventListener('DOMContentLoaded', function() {
 	console.log('DOM loaded');
 	console.log(sessionStorage.clientId);
-	getAPI(`https://localhost:44374/api/client/${sessionStorage.clientId}`);
 	GetDomElements();
+	getAPI(`https://localhost:44374/api/client/${sessionStorage.clientId}`);
 });
