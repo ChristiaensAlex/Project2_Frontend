05e95fb185bce99d3ebd7aee693f62f6e4b71f46
Alexandra <alexandra.christiaens@student.howest.be>
2020-01-27 18:18:21 +0100

    fixed bugs autocomplete + bugs adding multiple progressive schemes with same frequency

--- script/overviewAscribed.js
+++ script/overviewAscribed.js
@@ -1,35 +1,36 @@
-'use strict'
+'use strict';
 let frequency,
-  chosenDays = [],
-  baseURL = 'https://localhost:44374/api/',
-  //payload = [],
-  counter = 0,
-  chosenItems,
-  selects
-const postCP = function (url, payload) {
-  console.log('add client to progressive scheme')
-  let body = JSON.stringify(payload)
-  console.log(body)
-  fetch(`${baseURL}${url}`, {
-    method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: { 'Content-Type': 'application/json' },
-    body: body
-  })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data), console.log(data) //(window.location.href = backPage);
-    })
-    .catch(err => console.log(err))
-}
-const ShowInputs = function (items) {
-  let html = ''
-  for (let client of items.allChosenClients) {
-    html += `<div class="c-title">${client.firstName} ${client.lastName}</div>`
-    for (let ps of items.allChosenPs) {
-      html += `
+	chosenDays = [],
+	baseURL = 'https://localhost:44374/api/',
+	//payload = [],
+	counter = 0,
+	chosenItems,
+	numberOfDays = [],
+	selects;
+const postCP = function(url, payload) {
+	console.log('add client to progressive scheme');
+	let body = JSON.stringify(payload);
+	console.log(body);
+	fetch(`${baseURL}${url}`, {
+		method: 'POST',
+		mode: 'cors',
+		cache: 'no-cache',
+		credentials: 'same-origin',
+		headers: { 'Content-Type': 'application/json' },
+		body: body
+	})
+		.then(res => res.json())
+		.then(data => {
+			console.log(data), console.log(data); //(window.location.href = backPage);
+		})
+		.catch(err => console.log(err));
+};
+const ShowInputs = function(items) {
+	let html = '';
+	for (let client of items.allChosenClients) {
+		html += `<div class="c-title">${client.firstName} ${client.lastName}</div>`;
+		for (let ps of items.allChosenPs) {
+			html += `
             <div>${ps.name}</div>
             <p>
             <label class="c-label" for="points">Te verdienen muntjes bij het afronden van het stappenplan </label>
@@ -49,6 +50,7 @@ const ShowInputs = function (items) {
                     <path d="M7,10l5,5,5-5Z" />
                 </svg>
             </span>
+            <span class="js-specificFrequency"></span>
         </p>
 
         <!-- Hier begint de kalender die enkel getoont moet worden nadat je de herhaling hebt gekozen-->
@@ -77,170 +79,178 @@ const ShowInputs = function (items) {
         <span>Z</span>
     </div> -->
             </span>
+           
         </p>
         <p class="c-time">
             <label class="c-label" for="time">Uur: &nbsp</label>
             <input class="c-input js-time" type="time" id="time" name="time" placeholder="00:00" />
-        </p>`
-      counter++
-    }
-  }
-  document.querySelector('.js-frequency').innerHTML = html
-  selects = document.querySelectorAll('.js-select')
-  ListenToSelect(selects)
-}
-const makePayload = function (i, clientId, progressiveSchemeId) {
-  let points = document.querySelectorAll('.js-points')
-  let startDate = document.querySelectorAll('.js-startDate')
-  let endDate = document.querySelectorAll('.js-endDate')
-  let time = document.querySelectorAll('.js-time')
-  console.log(time)
-  let url
+        </p>`;
+			counter++;
+		}
+	}
+	document.querySelector('.js-frequency').innerHTML = html;
+	selects = document.querySelectorAll('.js-select');
+	ListenToSelect(selects);
+};
+const makePayload = function(i, clientId, progressiveSchemeId) {
+	let points = document.querySelectorAll('.js-points');
+	let startDate = document.querySelectorAll('.js-startDate');
+	let endDate = document.querySelectorAll('.js-endDate');
+	let time = document.querySelectorAll('.js-time');
+	console.log(time);
+	let url;
 
-  let frequency = selects[i].value
-  console.log(selects)
-  console.log(frequency)
-  switch (frequency) {
-    case 'Dagelijks':
-      url = 'clientprogressiveScheme/everyday'
-      let cpDaily = [
-        {
-          clientId: clientId,
-          progressiveSchemeId: progressiveSchemeId,
-          progressiveSchemeCoins: parseInt(points[i].value),
-          startDate: startDate[i].value,
-          endDate: endDate[i].value,
-          time: time[i].value
-        }
-      ]
-      //payload.push(cpDaily)
-      console.log(cpDaily)
-      postCP(url, cpDaily)
-      return url
-    case 'Wekelijks':
-      console.log(chosenDays)
-      url = 'clientprogressiveScheme/everyWeek'
+	let frequency = selects[i].value;
+	console.log(selects);
+	console.log(frequency);
+	switch (frequency) {
+		case 'Dagelijks':
+			url = 'clientprogressiveScheme/everyday';
+			let cpDaily = [
+				{
+					clientId: clientId,
+					progressiveSchemeId: progressiveSchemeId,
+					progressiveSchemeCoins: parseInt(points[i].value),
+					startDate: startDate[i].value,
+					endDate: endDate[i].value,
+					time: time[i].value
+				}
+			];
+			//payload.push(cpDaily)
+			console.log(cpDaily);
+			postCP(url, cpDaily);
+			return url;
+		case 'Wekelijks':
+			console.log(chosenDays);
+			url = 'clientprogressiveScheme/everyWeek';
 
-      let cpWeekly = [
-        {
-          clientId: clientId,
-          progressiveSchemeId: progressiveSchemeId,
-          progressiveSchemeCoins: parseInt(points[i].value),
-          startDate: startDate[i].value,
-          endDate: endDate[i].value,
-          time: time[i].value,
-          weekdays: chosenDays
-        }
-      ]
-      //payload.push(cpWeekly)
-      console.log(cpWeekly)
-      postCP(url, cpWeekly)
-      return url
-    case 'Elke ... dagen':
-      url = 'clientprogressiveScheme/everyFewDays'
+			let cpWeekly = [
+				{
+					clientId: clientId,
+					progressiveSchemeId: progressiveSchemeId,
+					progressiveSchemeCoins: parseInt(points[i].value),
+					startDate: startDate[i].value,
+					endDate: endDate[i].value,
+					time: time[i].value,
+					weekdays: chosenDays[i]
+				}
+			];
+			//payload.push(cpWeekly)
+			console.log(cpWeekly);
+			postCP(url, cpWeekly);
+			return url;
+		case 'Elke ... dagen':
+			url = 'clientprogressiveScheme/everyFewDays';
 
-      let cpEveryFewDays = [
-        {
-          numberOfDays: parseInt(numberOfDays.value),
-          clientId: clientId,
-          progressiveSchemeId: progressiveSchemeId,
-          progressiveSchemeCoins: parseInt(points[i].value),
-          startDate: startDate[i].value,
-          endDate: endDate[i].value,
-          time: time[i].value
-        }
-      ]
-      //payload.push(cpEveryFewDays)
-      console.log(cpEveryFewDays)
-      postCP(url, cpEveryFewDays)
+			let cpEveryFewDays = [
+				{
+					numberOfDays: parseInt(numberOfDays[i]),
+					clientId: clientId,
+					progressiveSchemeId: progressiveSchemeId,
+					progressiveSchemeCoins: parseInt(points[i].value),
+					startDate: startDate[i].value,
+					endDate: endDate[i].value,
+					time: time[i].value
+				}
+			];
+			//payload.push(cpEveryFewDays)
+			console.log(cpEveryFewDays);
+			postCP(url, cpEveryFewDays);
 
-      return url
-  }
+			return url;
+	}
 
-  // postCP(url, payload)
-}
+	// postCP(url, payload)
+};
 
-const getDayOfWeek = function (day) {
-  switch (day) {
-    case 'Ma':
-      return 'monday'
-    case 'Di':
-      return 'tuesday'
-    case 'Wo':
-      return 'wednesday'
-    case 'Do':
-      return 'thursday'
-    case 'Vr':
-      return 'friday'
-    case 'Za':
-      return 'saturday'
-    case 'Zo':
-      return 'sunday'
-  }
-}
-const ListenToSelect = function (selects) {
-  for (let select of selects) {
-    select.addEventListener('change', function () {
-      console.log(this.value)
-      frequency = this.value
-      switch (frequency) {
-        case 'Dagelijks':
-          return
-        case 'Elke ... dagen':
-          this.parentElement.innerHTML += `<div class="c-numberOfDays"><label class="c-label" for="numberOfDays">Elke&nbsp 
-                <input class="c-input c-numberOfDays-input js-numberOfDays" type="number" name="numberOfDays" min="1" max="6" placeholder="1 tot 6" /> &nbspdagen</label></div>`
-          let numberOfDaysInput = document.querySelector('.js-numberOfDays')
-          ListenToNumberOfDays(numberOfDaysInput)
-          return
-        case 'Wekelijks':
-          this.parentElement.innerHTML += `<div class="c-days"> <span class= "c-day" >Ma</span> <span class= "c-day">Di</span> <span class= "c-day">Wo</span> <span class= "c-day">Do</span> <span class= "c-day">Vr</span> <span class= "c-day">Za</span>
-                <span class= "c-day">Zo</span> </div>`
-          let allDays = document.querySelectorAll('.c-day')
-          console.log(allDays)
-          listenToDays(allDays)
-          return
-      }
-    })
-  }
-}
+const getDayOfWeek = function(day) {
+	switch (day) {
+		case 'Ma':
+			return 'monday';
+		case 'Di':
+			return 'tuesday';
+		case 'Wo':
+			return 'wednesday';
+		case 'Do':
+			return 'thursday';
+		case 'Vr':
+			return 'friday';
+		case 'Za':
+			return 'saturday';
+		case 'Zo':
+			return 'sunday';
+	}
+};
+const ListenToSelect = function(selects) {
+	for (let select of selects) {
+		select.addEventListener('change', function() {
+			console.log(this.value);
+			frequency = this.value;
+			switch (frequency) {
+				case 'Dagelijks':
+					return;
+				case 'Elke ... dagen':
+					//console.log(this.parentElement.parentElement);
+					//	console.log(this.parentElement.parentElement.querySelector('.js-specificFrequency'));
+					this.parentElement.parentElement.querySelector('.js-specificFrequency').innerHTML = `<div class="c-numberOfDays"><label class="c-label" for="numberOfDays">Elke&nbsp 
+                <input class="c-input c-numberOfDays-input js-numberOfDays" type="number" name="numberOfDays" min="1" max="6" placeholder="1 tot 6" /> &nbspdagen</label></div>`;
+					let numberOfDaysInput = document.querySelectorAll('.js-numberOfDays');
+					ListenToNumberOfDays(numberOfDaysInput);
+					return;
+				case 'Wekelijks':
+					this.parentElement.parentElement.querySelector('.js-specificFrequency').innerHTML = `<div class="c-days"> <span class= "c-day" >Ma</span> <span class= "c-day">Di</span> <span class= "c-day">Wo</span> <span class= "c-day">Do</span> <span class= "c-day">Vr</span> <span class= "c-day">Za</span>
+                <span class= "c-day">Zo</span> </div>`;
+					let allDays = document.querySelectorAll('.c-day');
+					console.log(allDays);
+					listenToDays(allDays);
+					return;
+			}
+		});
+	}
+};
 
-const ListenToNumberOfDays = function (number) {
-  number.addEventListener('change', function () {
-    let numberOfDays = this.value
-    console.log('numberofdays' + numberOfDays)
-  })
-}
-const listenToDays = function (days) {
-  for (let day of days) {
-    day.addEventListener('click', function () {
-      console.log(this.innerHTML)
-      let day = getDayOfWeek(this.innerHTML)
-      chosenDays.push(day)
+const ListenToNumberOfDays = function(numbers) {
+	for (let number of numbers) {
+		number.addEventListener('change', function() {
+			numberOfDays.push(this.value);
 
-      this.classList.add('c-selected')
-    })
-  }
-}
-const ListenToSubmit = function (button) {
-  button.addEventListener('click', function (e) {
-    e.preventDefault()
-    let i = 0
-    for (let client of chosenItems.allChosenClients) {
-      let clientId = client.id
-      for (let ps of chosenItems.allChosenPs) {
-        let progressiveSchemeId = ps.id
-        makePayload(i, clientId, progressiveSchemeId)
-        i++
-      }
-    }
-  })
-}
-document.addEventListener('DOMContentLoaded', function () {
-  console.log('DOM loaded - contact')
-  chosenItems = JSON.parse(sessionStorage.items)
-  console.log(chosenItems)
-  let submit = document.querySelector('.js-submitbutton')
-  ShowInputs(chosenItems)
+			console.log('numberofdays' + numberOfDays);
+		});
+	}
+};
+const listenToDays = function(days) {
+	let clickedDays = [];
+	for (let day of days) {
+		day.addEventListener('click', function() {
+			console.log(this.innerHTML);
+			let day = getDayOfWeek(this.innerHTML);
+			clickedDays.push(day);
 
-  ListenToSubmit(submit)
-})
+			this.classList.add('c-selected');
+		});
+	}
+	chosenDays.push(clickedDays);
+};
+const ListenToSubmit = function(button) {
+	button.addEventListener('click', function(e) {
+		e.preventDefault();
+		let i = 0;
+		for (let client of chosenItems.allChosenClients) {
+			let clientId = client.id;
+			for (let ps of chosenItems.allChosenPs) {
+				let progressiveSchemeId = ps.id;
+				makePayload(i, clientId, progressiveSchemeId);
+				i++;
+			}
+		}
+	});
+};
+document.addEventListener('DOMContentLoaded', function() {
+	console.log('DOM loaded - contact');
+	chosenItems = JSON.parse(sessionStorage.items);
+	console.log(chosenItems);
+	let submit = document.querySelector('.js-submitbutton');
+	ShowInputs(chosenItems);
+
+	ListenToSubmit(submit);
+});
---
+++ script/picture.js
@@ -0,0 +1,182 @@
+let mainPictoFile, pictoTags, pictos, picto;  
+const postMainPicto = function(file, tags){
+    console.log("post"); 
+    console.log(file); 
+    var formData = new FormData(); 
+    formData.append("tags", tags); 
+    formData.append("file", file); 
+    let url = `${baseURL}picto`
+    
+    
+    fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        body: formData
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data); 
+            })
+            .catch(err => console.log(err));
+    };
+
+const init = function(){
+    const mainPicto = document.querySelector('.js-uploadMainPicto'); 
+    console.log(mainPicto); 
+    if(mainPicto){
+    mainPicto.addEventListener('change', function(){
+        mainPictoFile = this.files[0]; 
+        console.log(mainPictoFile)
+    })
+    const pictoTag = document.querySelector('.js-tagInput'); 
+    console.log(pictoTag);
+    pictoTag.addEventListener('blur', function(){
+        console.log(mainPictoFile);
+        tags = this.value; 
+        console.log(mainPictoFile); 
+        if(tags.indexOf(',')> -1){
+            let arr = tags.split(",");
+            let tagArr = []; 
+            for(i in arr){
+                tagArr.push(arr[i]); 
+            }
+            pictoTags = tagArr; 
+            console.log(tagArr)
+        }
+        else{
+            pictoTags = tags;
+            console.log(pictoTags)
+        }
+        if(pictoTags && mainPictoFile){
+            postMainPicto(mainPictoFile, pictoTags); 
+        }
+    })
+}
+}
+
+const listenToSelect = function(json){
+    let pictos = document.querySelectorAll('.c-choose__picto'); 
+    for(eachPicto of pictos){
+        eachPicto.addEventListener('click', function(){
+            this.innerHTML += `<div class="c-picto__selected c-checkmark">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20.278" height="19.963" viewBox="0 0 20.278 19.963">
+              <g id="Group_1013" data-name="Group 1013" transform="translate(-325.12 -355)">
+                <ellipse id="Ellipse_128" data-name="Ellipse 128" cx="10.139" cy="9.981" rx="10.139" ry="9.981" transform="translate(325.12 355)" fill="#1c5c5c" />
+                <path id="Fill_1" data-name="Fill 1" d="M4.251,8.665a.925.925,0,0,1-.663-.28L.275,5.008a.968.968,0,0,1,0-1.351.925.925,0,0,1,1.326,0l2.651,2.7L10.216.28a.925.925,0,0,1,1.325,0,.968.968,0,0,1,0,1.351L4.914,8.385a.926.926,0,0,1-.663.28" transform="translate(328.828 360.29)" fill="#fff" />
+              </g>
+            </svg>
+          </div>`;
+          let i = this.getAttribute('pictonr'); 
+          let mainPictoName = json[i].name; 
+          sessionStorage.mainPictoName = mainPictoName; 
+          console.log(sessionStorage.mainPictoName)
+        })
+    }
+    listenToSelectSubmit();
+}
+
+const showPictos = function(json){
+    for(i in json){
+        let pictoClone = picto.cloneNode(true); 
+        pictoClone.classList.remove('u-hide'); 
+        let pictoC = json[i]
+        let pictoSource = pictoClone.querySelector('.c-choose__picto-img');
+        console.log(pictoSource); 
+        pictoClone.setAttribute('pictonr', i); 
+        console.log(pictoClone.getAttribute('pictonr')); 
+        pictoSource.src = `https://trekjeplan.blob.core.windows.net/pictos/${pictoC.name}`; 
+        pictos.appendChild(pictoClone); 
+    }
+    listenToSelect(json);
+}
+
+const showFilteredPictos = function(json){
+    console.log("Gefilterde");
+    pictos.innerHTML = ''; 
+    for(i in json){
+        let pictoClone = picto.cloneNode(true); 
+        pictoClone.classList.remove('u-hide'); 
+        let pictoC = json[i]
+        let pictoSource = pictoClone.querySelector('.c-choose__picto-img');
+        console.log(pictoSource); 
+        pictoClone.setAttribute('pictonr', i); 
+        console.log(pictoClone.getAttribute('pictonr')); 
+        pictoSource.src = `https://trekjeplan.blob.core.windows.net/pictos/${pictoC.name}`; 
+        pictos.appendChild(pictoClone); 
+    }
+    listenToSelect(json); 
+}
+
+const getPictos = function(url, filtered){
+    console.log("get"); 
+    console.log(url)
+    fetch(url)
+    .then(function(response) {
+        if (!response.ok) {
+            throw Error(`Problem to fetch(). Status code: ${response.status}`);
+        } else {
+            return response.json();
+        }
+    })
+		.then(function(jsonObject) {
+			json = jsonObject;
+            console.log(jsonObject);
+            console.log("Filtered" + filtered)
+            if(jsonObject){
+                if(filtered == true){
+                    showFilteredPictos(json);
+                }
+                else if(filtered == false){
+            showPictos(json); }}
+			//showContacts(jsonObject);
+		})
+		.catch(function(error) {
+			console.log(error);
+			console.error(`Problem to process json $`);
+		});
+}
+
+const listenToSearch = function(){
+    let searchPicto = document.querySelector('.c-input-search'); 
+    console.log(searchPicto)
+    searchPicto.addEventListener('input', function(){
+        console.log("Er verandert hier gelijk iets");
+        let url = `${baseURL}picto?search=${searchPicto.value}`; 
+        getPictos(url, true); 
+    })
+}
+
+const fillMainPicto = function(){
+    console.log(sessionStorage.mainPictoName); 
+    if(document.title == 'Trek Je Plan - Maak een nieuw stappenplan aan'){
+        console.log("in if")
+        let mainPictoImage = document.querySelector('.c-button__mainStepImage'); 
+        console.log(mainPictoImage)
+        mainPictoImage.innerHTML=`<img src="https://trekjeplan.blob.core.windows.net/pictos/${sessionStorage.mainPictoName}" class='js-mainPicto' >`
+    }
+}
+const showMainPicto = function(){
+    window.location.href = 'CreateProgressiveScheme.html'; 
+}
+
+const listenToSelectSubmit = function(){
+    let selectSubmit = document.querySelector('.c-submitbutton'); 
+    selectSubmit.addEventListener('click', showMainPicto)
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('DOM loaded - Picture'); 
+    let url = `${baseURL}picto`;
+    pictos = document.querySelector('.c-choose'); 
+    picto = document.querySelector('.c-choose__picto');
+    init(); 
+    
+    if(document.title =='Trek Je Plan - Kies een afbeelding'){
+        getPictos(url, false); 
+    listenToSearch();
+}
+else if(document.title == 'Trek Je Plan - Maak een nieuw stappenplan aan'){
+fillMainPicto(); }
+})
