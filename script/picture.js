ed21cd020cd41cc109c0ec5f6ecf478f0fec8bba
Shannon <shannon.goemaere@student.howest.be>
2020-01-27 02:12:48 +0100

    Selected mainPicto shown

--- script/picture.js
+++ script/picture.js
@@ -1,5 +1,4 @@
-let mainPictoFile, pictoTags; 
-let baseURL = 'https://localhost:44374/api/'; 
+let mainPictoFile, pictoTags, pictos, picto;  
 const postMainPicto = function(file, tags){
     console.log("post"); 
     console.log(file); 
@@ -26,6 +25,7 @@ const postMainPicto = function(file, tags){
 const init = function(){
     const mainPicto = document.querySelector('.js-uploadMainPicto'); 
     console.log(mainPicto); 
+    if(mainPicto){
     mainPicto.addEventListener('change', function(){
         mainPictoFile = this.files[0]; 
         console.log(mainPictoFile)
@@ -54,25 +54,46 @@ const init = function(){
         }
     })
 }
+}
+
+const listenToSelect = function(json){
+    let pictos = document.querySelectorAll('.c-choose__picto'); 
+    for(eachPicto of pictos){
+        eachPicto.addEventListener('click', function(){
+            this.innerHTML += `<div class="c-picto__selected c-checkmark">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20.278" height="19.963" viewBox="0 0 20.278 19.963">
+              <g id="Group_1013" data-name="Group 1013" transform="translate(-325.12 -355)">
+                <ellipse id="Ellipse_128" data-name="Ellipse 128" cx="10.139" cy="9.981" rx="10.139" ry="9.981" transform="translate(325.12 355)" fill="#1c5c5c" />
+                <path id="Fill_1" data-name="Fill 1" d="M4.251,8.665a.925.925,0,0,1-.663-.28L.275,5.008a.968.968,0,0,1,0-1.351.925.925,0,0,1,1.326,0l2.651,2.7L10.216.28a.925.925,0,0,1,1.325,0,.968.968,0,0,1,0,1.351L4.914,8.385a.926.926,0,0,1-.663.28" transform="translate(328.828 360.29)" fill="#fff" />
+              </g>
+            </svg>
+          </div>`;
+          let i = this.getAttribute('pictonr'); 
+          let mainPictoName = json[i].name; 
+          sessionStorage.mainPictoName = mainPictoName; 
+          console.log(sessionStorage.mainPictoName)
+        })
+    }
+    listenToSelectSubmit();
+}
 
 const showPictos = function(json){
     for(i in json){
-        let pictos = document.querySelector('.c-choose'); 
-        let picto = document.querySelector('.c-choose__picto');
         let pictoClone = picto.cloneNode(true); 
         pictoClone.classList.remove('u-hide'); 
         let pictoC = json[i]
         let pictoSource = pictoClone.querySelector('.c-choose__picto-img');
         console.log(pictoSource); 
+        pictoClone.setAttribute('pictonr', i); 
+        console.log(pictoClone.getAttribute('pictonr')); 
         pictoSource.src = `https://trekjeplan.blob.core.windows.net/pictos/${pictoC.name}`; 
         pictos.appendChild(pictoClone); 
     }
+    listenToSelect(json);
 }
 
 const showFilteredPictos = function(json){
     console.log("Gefilterde");
-    let picto = document.querySelector('.c-choose__picto');
-    let pictos = document.querySelector('.c-choose'); 
     pictos.innerHTML = ''; 
     for(i in json){
         let pictoClone = picto.cloneNode(true); 
@@ -80,9 +101,12 @@ const showFilteredPictos = function(json){
         let pictoC = json[i]
         let pictoSource = pictoClone.querySelector('.c-choose__picto-img');
         console.log(pictoSource); 
+        pictoClone.setAttribute('pictonr', i); 
+        console.log(pictoClone.getAttribute('pictonr')); 
         pictoSource.src = `https://trekjeplan.blob.core.windows.net/pictos/${pictoC.name}`; 
         pictos.appendChild(pictoClone); 
     }
+    listenToSelect(json); 
 }
 
 const getPictos = function(url, filtered){
@@ -124,10 +148,35 @@ const listenToSearch = function(){
     })
 }
 
+const fillMainPicto = function(){
+    console.log(sessionStorage.mainPictoName); 
+    if(document.title == 'Trek Je Plan - Maak een nieuw stappenplan aan'){
+        console.log("in if")
+        let mainPictoImage = document.querySelector('.c-button__mainStepImage'); 
+        console.log(mainPictoImage)
+        mainPictoImage.innerHTML=`<img src="https://trekjeplan.blob.core.windows.net/pictos/${sessionStorage.mainPictoName}" class='js-mainPicto' >`
+    }
+}
+const showMainPicto = function(){
+    window.location.href = 'CreateProgressiveScheme.html'; 
+}
+
+const listenToSelectSubmit = function(){
+    let selectSubmit = document.querySelector('.c-submitbutton'); 
+    selectSubmit.addEventListener('click', showMainPicto)
+}
+
 document.addEventListener('DOMContentLoaded', function(){
     console.log('DOM loaded - Picture'); 
     let url = `${baseURL}picto`;
+    pictos = document.querySelector('.c-choose'); 
+    picto = document.querySelector('.c-choose__picto');
     init(); 
-    getPictos(url, false); 
+    
+    if(document.title =='Trek Je Plan - Kies een afbeelding'){
+        getPictos(url, false); 
     listenToSearch();
+}
+else if(document.title == 'Trek Je Plan - Maak een nieuw stappenplan aan'){
+fillMainPicto(); }
 })
