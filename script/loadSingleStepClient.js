02372b8931df0751e2a0237f9ea5a1917e7bdafd
Alexandra <alexandra.christiaens@student.howest.be>
2020-01-27 20:25:13 +0100

    client can view progressiveschemes of the day + complete see every step and complete progressive scheme

--- script/loadSingleStepClient.js
+++ script/loadSingleStepClient.js
@@ -2,76 +2,74 @@ baseURL = 'https://localhost:44374/api/';
 var beginning;
 var end;
 let step;
-let activeIndex; 
+let activeIndex, startTouch;
 
-let progressiveStepPlanBeginning = function (isBeginning) {
-    beginning = isBeginning;
+let progressiveStepPlanBeginning = function(isBeginning) {
+	beginning = isBeginning;
 };
 
-let progressiveStepPlanEnd = function (isEnd) {
-    end = isEnd;
+let progressiveStepPlanEnd = function(isEnd) {
+	end = isEnd;
 };
 
-const putStepFullFilled = function(){
-    // currentStep --> stap ervoor is dus checked meegeven in body 
-    let url = `${baseURL}client/progressiveScheme/${clientProgressiveSchemeId}`; 
-    fetch(url, {
+const putStepFullFilled = function() {
+	// currentStep --> stap ervoor is dus checked meegeven in body
+	let url = `${baseURL}client/progressiveScheme/${clientProgressiveSchemeId}`;
+	fetch(url, {
 		method: 'PUT',
 		mode: 'cors',
 		cache: 'no-cache',
 		credentials: 'same-origin',
-		headers: { 'Content-Type': 'application/json' },
+		headers: { 'Content-Type': 'application/json' }
 	})
 		.then(data => {
-            console.log(data); 
-            getSteps(clientProgressiveSchemeId)
+			console.log(data);
+			getSteps(clientProgressiveSchemeId);
 		})
 		.catch(err => console.log(err));
-}
+};
 
-const putStartTime = function(){
-
-    let url = `${baseURL}client/progressiveScheme/${clientProgressiveSchemeId}/startTime`; 
-    fetch(url, {
+const putStartTime = function() {
+	let url = `${baseURL}client/progressiveScheme/${clientProgressiveSchemeId}/startTime`;
+	fetch(url, {
 		method: 'PUT',
 		mode: 'cors',
 		cache: 'no-cache',
 		credentials: 'same-origin',
-		headers: { 'Content-Type': 'application/json' },
+		headers: { 'Content-Type': 'application/json' }
 	})
 		.then(data => {
-            console.log(data);
-            getSteps(clientProgressiveSchemeId)
+			console.log(data);
+			getSteps(clientProgressiveSchemeId);
 		})
 		.catch(err => console.log(err));
-}
+};
 
-const stepNumber = function () {
-    console.log("Begin: " + beginning);
-    console.log("Eind: " + end); 
-    if (beginning == true) {
-        step = 1;
-        end = true; 
-        putStartTime(); 
-        putStepFullFilled(); 
-    }
-        //putStepFullFilled(activeIndex);
-    else {
-        step = activeIndex + 1;
-        putStepFullFilled(); 
-    };
-    // step 1 is activeIndex = 0;
-    console.log("Begin: " + beginning);
-    console.log("Eind: " + end); 
-}
+const stepNumber = function() {
+	console.log('Begin: ' + beginning);
+	console.log('Eind: ' + end);
+	if (beginning == true) {
+		step = 1;
+		end = true;
+		putStartTime();
+		putStepFullFilled();
+	}
+	//putStepFullFilled(activeIndex);
+	else {
+		step = activeIndex + 1;
+		putStepFullFilled();
+	}
+	// step 1 is activeIndex = 0;
+	console.log('Begin: ' + beginning);
+	console.log('Eind: ' + end);
+};
 
-
-const showStepsClient = function (json) {
-    let html = '';
-    let title = document.querySelector('.c-progressiveScheme__name'); 
-    title.innerHTML = json.name; 
-    for (object of json.steps) {
-        html += `<div class="swiper-slide">
+const showStepsClient = function(json) {
+	let html = '';
+	let title = document.querySelector('.c-progressiveScheme__name');
+	title.innerHTML = json.name;
+	for (object of json.steps) {
+		html += `<div class="swiper-slide">
 						<div class="c-step">
 							<div class="c-step__pictowrapper">
 								<div class="c-step__picto">
@@ -83,63 +81,82 @@ const showStepsClient = function (json) {
 								${object.descriptionStep}
 							</p>
 						</div>
-					</div>`;
-    }
-    document.querySelector('.js-steps').innerHTML = html;
-    mySwiper.update();
-    let startTouch; 
-    let nextTouch; 
-    if (end == true){
-        console.log("Einde touch"); 
-    mySwiper.on('touchStart', function(e){
-        console.log("Start")
-        console.log(e);
-        startTouch = e.screenX; 
-        console.log(startTouch)
-    })
-    mySwiper.on('touchMove', function(e){
-        console.log(e);
-        if (startTouch > e.screenX){
-            window.location.href = 'FinishedProgressiveScheme.html';
-        }
-        
-    })
-}
-   
+                    </div>`;
+	}
+
+	document.querySelector('.swiper-wrapper').innerHTML = html;
+	mySwiper.update();
+	mySwiper.on('reachBeginning', function(e) {
+		console.log(e);
+	});
+	// let startTouch;
+	// let nextTouch;
+	// if (end == true) {
+	// 	console.log('Einde touch');
+	// 	mySwiper.on('touchStart', function(e) {
+	// 		console.log('Start');
+	// 		console.log(e);
+	// 		startTouch = e.screenX;
+	// 		console.log(startTouch);
+	// 	});
+	// 	mySwiper.on('touchMove', function(e) {
+	// 		console.log('beweging');
+	// 		console.log(e);
+	// 		if (startTouch > e.screenX) {
+	// 			console.log('eventje rechts');
+	// 			window.location.href = 'FinishedProgressiveScheme.html';
+	// 		}
+	// 	});
+	// }
 };
 
+mySwiper.on('reachEnd', function() {
+	console.log('einde'), console.log();
+	console.log('Einde touch');
+	mySwiper.on('touchStart', function(e) {
+		console.log('Start');
+		console.log(e);
+		startTouch = e.touches[0].screenX;
+		console.log(startTouch);
+	});
+	mySwiper.on('touchMove', function(e) {
+		console.log('beweging');
+		console.log(e);
+		if (startTouch > e.touches[0].screenX) {
+			console.log('eventje rechts');
+			window.location.href = 'FinishedProgressiveScheme.html';
+		}
+	});
+});
 
-const getSteps = function (clientProgressiveSchemeId) {
-    let url = `${baseURL}client/progressiveScheme/${clientProgressiveSchemeId}`;
-    fetch(url)
-        .then(function (response) {
-            if (!response.ok) {
-                throw Error(`Problem to fetch(). Status code: ${response.status}`);
-            } else {
-                let arr = new Array();
-                arr = response.json();
-                console.log(arr);
-                return arr;
-            }
-        })
-        .then(function (jsonObject) {
-            json = jsonObject;
-            if (json.totalSteps == 1){
-                end = true; 
-            }
-            showStepsClient(json);
-            
-        })
-        .catch(function (error) {
-            console.error(`Problem to process json ${error}`);
-        });
-       
+const getSteps = function(clientProgressiveSchemeId) {
+	let url = `${baseURL}client/progressiveScheme/${clientProgressiveSchemeId}`;
+	fetch(url)
+		.then(function(response) {
+			if (!response.ok) {
+				throw Error(`Problem to fetch(). Status code: ${response.status}`);
+			} else {
+				let arr = new Array();
+				arr = response.json();
+				console.log(arr);
+				return arr;
+			}
+		})
+		.then(function(jsonObject) {
+			json = jsonObject;
+			if (json.totalSteps == 1) {
+				end = true;
+			}
+			showStepsClient(json);
+		})
+		.catch(function(error) {
+			console.error(`Problem to process json ${error}`);
+		});
 };
 
-
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('DOM loaded - SingleStepsClient');
-    clientProgressiveSchemeId = sessionStorage.clientSchemeId;
-    getSteps(clientProgressiveSchemeId);
-    stepNumber();
+document.addEventListener('DOMContentLoaded', function() {
+	console.log('DOM loaded - SingleStepsClient');
+	clientProgressiveSchemeId = sessionStorage.clientSchemeId;
+	getSteps(clientProgressiveSchemeId);
+	stepNumber();
 });
