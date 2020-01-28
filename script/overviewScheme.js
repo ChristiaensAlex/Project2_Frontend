4cdb4af57fa7addf9fda1768436c4e0076d78030
Shannon <shannon.goemaere@student.howest.be>
2020-01-28 14:17:37 +0100

    Fixed search on picture tags

--- script/overviewScheme.js
+++ script/overviewScheme.js
@@ -1,97 +1,98 @@
-let json
+let json;
 
-const showStepsFromProgressiveScheme = function (payload) {
-  let steps = payload.steps
-  let progressiveSchemeSteps = document.querySelector('.js-scheme-allSteps')
+const showStepsFromProgressiveScheme = function(payload) {
+  let steps = payload.steps;
+  let progressiveSchemeSteps = document.querySelector('.js-scheme-allSteps');
   for (i in steps) {
-    console.log(steps)
-    let OneStep = document.querySelector('.js-overview-step')
-    let stepClone = OneStep.cloneNode(true)
-    stepClone.classList.remove('u-hide')
-    stepClone.classList.remove('js-overview-step')
-    let step = steps[i]
-    let stepName = stepClone.querySelector('.c-step__name')
-    stepName.innerHTML = `Stap ${step.sequence}`
-    let stepDescription = stepClone.querySelector('.c-step__explanation-mentor')
-    stepDescription.innerHTML = step.descriptionStep
-    
+    console.log(steps);
+    let OneStep = document.querySelector('.js-overview-step');
+    let stepClone = OneStep.cloneNode(true);
+    stepClone.classList.remove('u-hide');
+    stepClone.classList.remove('js-overview-step');
+    let step = steps[i];
+    let stepName = stepClone.querySelector('.c-step__name');
+    stepName.innerHTML = `Stap ${step.sequence}`;
+    let stepDescription = stepClone.querySelector('.c-step__explanation-mentor');
+    stepDescription.innerHTML = step.descriptionStep;
+
     let pictoStep = stepClone.querySelector('.c-picto');
-    pictoStep.innerHTML = `<img class="c-choose__picto-img" src="https://trekjeplan.blob.core.windows.net/pictos/${steps[i].pictoFilleName}" width="104px" height="auto"/>`; 
-    console.log(pictoStep); 
-    progressiveSchemeSteps.append(stepClone)
-    let count = steps.length - 1
+    pictoStep.innerHTML = `<img class="c-choose__picto-img" src="https://trekjeplan.blob.core.windows.net/pictos/${steps[i].pictoFilleName}" width="104px" height="auto"/>`;
+    console.log(pictoStep);
+    progressiveSchemeSteps.append(stepClone);
+    let count = steps.length - 1;
     if (i != steps.length - 1) {
-      let OneArrow = document.querySelector('.c-downwardsArrow')
-      let arrowClone = OneArrow.cloneNode(true)
-      arrowClone.classList.remove('u-hide')
-      progressiveSchemeSteps.append(arrowClone)
+      let OneArrow = document.querySelector('.c-downwardsArrow');
+      let arrowClone = OneArrow.cloneNode(true);
+      arrowClone.classList.remove('u-hide');
+      progressiveSchemeSteps.append(arrowClone);
     }
   }
   showClientsFromProgressiveScheme(payload);
-}
+};
 
-const fillInputsFromEditPlan = function (payload) {
-  let steps = payload.steps
-  let progressiveSchemeSteps = document.querySelector('.js-edit-scheme')
+const fillInputsFromEditPlan = function(payload) {
+  let steps = payload.steps;
+  let progressiveSchemeSteps = document.querySelector('.js-edit-scheme');
   for (i in steps) {
-    let OneStep = document.querySelector('.c-first-step')
-    let stepClone = OneStep.cloneNode(true)
-    stepClone.classList.remove('u-hide')
-    let step = steps[i]
-    stepClone.dataset.number = parseInt(step.sequence)
-    let stepNumber = stepClone.querySelector('.js-step-number')
+    let OneStep = document.querySelector('.c-first-step');
+    let stepClone = OneStep.cloneNode(true);
+    stepClone.classList.remove('u-hide');
+    let step = steps[i];
+    stepClone.dataset.number = parseInt(step.sequence);
+    let stepNumber = stepClone.querySelector('.js-step-number');
 
-    stepNumber.innerHTML = `Stap ${step.sequence}`
-    let stepInputDescription = stepClone.querySelector('.js-input-description')
-    stepInputDescription.innerHTML = step.descriptionStep
-    let imgdiv = stepClone.querySelector('.js-step-img')
+    stepNumber.innerHTML = `Stap ${step.sequence}`;
+    let stepInputDescription = stepClone.querySelector('.js-input-description');
+    stepInputDescription.innerHTML = step.descriptionStep;
+    let imgdiv = stepClone.querySelector('.js-step-img');
 
-    imgdiv.innerHTML = `<img class="c-selectedPicto js-selected-picto" src="https://trekjeplan.blob.core.windows.net/pictos/${step.pictoFilleName}" width="104px" height="auto" data-img="${step.pictoFilleName}"/>`
-    progressiveSchemeSteps.append(stepClone)
-
+    imgdiv.innerHTML = `<img class="c-selectedPicto js-selected-picto" src="https://trekjeplan.blob.core.windows.net/pictos/${step.pictoFilleName}" width="104px" height="auto" data-img="${step.pictoFilleName}"/>`;
+    progressiveSchemeSteps.append(stepClone);
   }
-  ListenToRemoveButton()
-  getElements(); 
-  buttonAddStep.addEventListener('click', AddToEditScheme)
-}
-const showOneProgressiveScheme = function (payload) {
-  json = payload
-  let progressiveSchemeName = document.querySelector(
-    '.js-progressiveScheme-name'
-  )
-  let editPlanName = document.querySelector('.js-scheme-name')
+  ListenToRemoveButton();
+  getElements();
+  buttonAddStep.addEventListener('click', AddToEditScheme);
+};
+const showOneProgressiveScheme = function(payload) {
+  json = payload;
+  let progressiveSchemeName = document.querySelector('.js-progressiveScheme-name');
+  let editPlanName = document.querySelector('.js-scheme-name');
   if (progressiveSchemeName) {
-    console.log(payload)
-    progressiveSchemeName.innerHTML = payload.name
-    showStepsFromProgressiveScheme(payload)
+    console.log(payload);
+    progressiveSchemeName.innerHTML = payload.name;
+    showStepsFromProgressiveScheme(payload);
   } else if (editPlanName) {
-    editPlanName.value = payload.name
-    document.querySelector(".js-mainImg").innerHTML = `<img class="c-selectedPicto js-selected-picto" src="https://trekjeplan.blob.core.windows.net/pictos/${payload.pictoFilleName}" width="104px" height="auto" data-img="${payload.pictoFilleName}"/>`
-    fillInputsFromEditPlan(payload)
+    editPlanName.value = payload.name;
+    document.querySelector('.js-mainImg').innerHTML = `<img class="c-selectedPicto js-selected-picto" src="https://trekjeplan.blob.core.windows.net/pictos/${payload.pictoFilleName}" width="104px" height="auto" data-img="${payload.pictoFilleName}"/>`;
+    fillInputsFromEditPlan(payload);
   }
-}
+};
 
-const getProgressiveSchemeById = function () {
-  let id = sessionStorage.planId
-  let url = `${baseURL}progressiveScheme/${id}`
+const getProgressiveSchemeById = function() {
+  let id = sessionStorage.planId;
+  let url = `${baseURL}progressiveScheme/${id}`;
   fetch(url)
-    .then(function (response) {
+    .then(function(response) {
       if (!response.ok) {
-        throw Error(`Problem to fetch(). Status code: ${response.status}`)
+        throw Error(`Problem to fetch(). Status code: ${response.status}`);
       } else {
-        return response.json()
+        return response.json();
       }
     })
-    .then(function (jsonObject) {
-      showOneProgressiveScheme(jsonObject)
+    .then(function(jsonObject) {
+      showOneProgressiveScheme(jsonObject);
     })
-    .catch(function (error) {
-      console.error(`Problem to process json ${error}`)
-    })
-}
+    .catch(function(error) {
+      console.error(`Problem to process json ${error}`);
+    });
+};
 
-document.addEventListener('DOMContentLoaded', function () {
-  console.log('DOM loaded - Overview progressive scheme ')
-  baseURL = 'https://trekjeplan.azurewebsites.net/api/'
-  getProgressiveSchemeById()
-})
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('DOM loaded - Overview progressive scheme ');
+  baseURL = 'https://trekjeplan.azurewebsites.net/api/';
+  console.log(document.title);
+  if (document.title == 'Trek Je Plan - Overzicht stappen in stappenplan - Mentor') {
+    console.log('IN IF ');
+    getProgressiveSchemeById();
+  }
+});
--- script/picto.js
+++ script/picto.js
@@ -1,79 +1,76 @@
-let mainPictoFile, pictoTags, tags; 
+let mainPictoFile, pictoTags, tags;
 
-const listenToSubmit = function(pictureName){
-    console.log("Luister naar submit")
-}
-
-const postMainPicto = function(file, tags){
-    var formData = new FormData(); 
-    formData.append("tags", tags); 
-    formData.append("file", file); 
-    let url = `${baseURL}picto`
-    
-    
-    fetch(url, {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        body: formData
-        })
-            .then(res => res.json())
-            .then(data => {
-
-                listenToSubmit(data.imageName); 
-                showPictos(data); 
-            })
-            .catch(err => console.log(err));
+const listenToSubmit = function(pictureName) {
+  console.log('Luister naar submit');
 };
 
-const init = function(){
-    const mainPicto = document.querySelector('.js-uploadMainPicto'); 
-    if(mainPicto){
-    mainPicto.addEventListener('change', function(){
-        mainPictoFile = this.files[0]; 
+const postMainPicto = function(file, tags) {
+  var formData = new FormData();
+  formData.append('tags', tags);
+  formData.append('file', file);
+  let url = `${baseURL}picto`;
+
+  fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    body: formData
+  })
+    .then(res => res.json())
+    .then(data => {
+      listenToSubmit(data.imageName);
+      showPictos(data);
     })
-    const pictoTag = document.querySelector('.js-tagInput'); 
-    pictoTag.addEventListener('blur', function(){
-        tags = this.value; 
-        if(tags.indexOf(',')> -1){
-            let arr = tags.split(",");
-            let tagArr = []; 
-            for(i in arr){
-                tagArr.push(arr[i]); 
-            }
-            pictoTags = tagArr; 
+    .catch(err => console.log(err));
+};
+
+const init = function() {
+  const mainPicto = document.querySelector('.js-uploadMainPicto');
+  if (mainPicto) {
+    mainPicto.addEventListener('change', function() {
+      mainPictoFile = this.files[0];
+    });
+    const pictoTag = document.querySelector('.js-tagInput');
+    pictoTag.addEventListener('blur', function() {
+      tags = this.value;
+      if (tags.indexOf(',') > -1) {
+        let arr = tags.split(',');
+        let tagArr = [];
+        for (i in arr) {
+          tagArr.push(arr[i]);
         }
-        else{
-            pictoTags = tags;
-        }
-        if(pictoTags && mainPictoFile){
-            postMainPicto(mainPictoFile, pictoTags); 
-        }
-    })
-}
-}
+        pictoTags = tagArr;
+      } else {
+        pictoTags = tags;
+      }
+      if (pictoTags && mainPictoFile) {
+        postMainPicto(mainPictoFile, pictoTags);
+      }
+    });
+  }
+};
 
-const OnHandlerClickedShowPicto = function(imgSource, dataImg){
-    background.classList.remove('c-popup-blur');
-    popup.style.display = 'none';
-    console.log(divElement);
-    divElement.innerHTML = `<img class="c-selectedPicto js-selected-picto" src="${imgSource}" width="104px" height="auto" data-img="${dataImg}"/>`
-}
+const OnHandlerClickedShowPicto = function(imgSource, dataImg) {
+  background.classList.remove('c-popup-blur');
+  popup.style.display = 'none';
+  console.log(divElement);
+  divElement.innerHTML = `<img class="c-selectedPicto js-selected-picto" src="${imgSource}" width="104px" height="auto" data-img="${dataImg}"/>`;
+};
 
-const listenToSelectSubmit = function(selectedPicto){
-    let selected = selectedPicto.querySelector('.c-choose__picto-img');
-    let selectSubmit = document.querySelector('.c-submitbutton-picto'); 
-    selectSubmit.addEventListener('click', function(){
-        OnHandlerClickedShowPicto(selected.src, selected.dataset.img)}); 
-}
+const listenToSelectSubmit = function(selectedPicto) {
+  let selected = selectedPicto.querySelector('.c-choose__picto-img');
+  let selectSubmit = document.querySelector('.c-submitbutton-picto');
+  selectSubmit.addEventListener('click', function() {
+    OnHandlerClickedShowPicto(selected.src, selected.dataset.img);
+  });
+};
 
-
-const listenToSelect = function(json){
-    let pictos = document.querySelectorAll('.c-choose__picto'); 
-    for(eachPicto of pictos){
-        eachPicto.addEventListener('click', function(){
-            this.innerHTML += `<div class="c-picto__selected c-checkmark">
+const listenToSelect = function(json) {
+  let pictos = document.querySelectorAll('.c-choose__picto');
+  for (eachPicto of pictos) {
+    eachPicto.addEventListener('click', function() {
+      this.innerHTML += `<div class="c-picto__selected c-checkmark">
             <svg xmlns="http://www.w3.org/2000/svg" width="20.278" height="19.963" viewBox="0 0 20.278 19.963">
               <g id="Group_1013" data-name="Group 1013" transform="translate(-325.12 -355)">
                 <ellipse id="Ellipse_128" data-name="Ellipse 128" cx="10.139" cy="9.981" rx="10.139" ry="9.981" transform="translate(325.12 355)" fill="#1c5c5c" />
@@ -81,80 +78,95 @@ const listenToSelect = function(json){
               </g>
             </svg>
           </div>`;
-          let i = this.getAttribute('pictonr'); 
-          if (sessionStorage.getItem("clickedStepDataNumber")){
-            let StepPictoName = json[i].name; 
-            sessionStorage.StepPictoName = StepPictoName; 
-          }else {
-            let mainPictoName = json[i].name; 
-            sessionStorage.mainPictoName = mainPictoName; 
-          }
+      let i = this.getAttribute('pictonr');
+      if (sessionStorage.getItem('clickedStepDataNumber')) {
+        let StepPictoName = json[i].name;
+        sessionStorage.StepPictoName = StepPictoName;
+      } else {
+        let mainPictoName = json[i].name;
+        sessionStorage.mainPictoName = mainPictoName;
+      }
 
-          listenToSelectSubmit(this);
-        })
+      listenToSelectSubmit(this);
+    });
+  }
+};
+
+const showPictos = function(json) {
+  for (i in json) {
+    let pictoClone = picto.cloneNode(true);
+    pictoClone.classList.remove('u-hide');
+    let pictoC = json[i];
+    let pictoSource = pictoClone.querySelector('.c-choose__picto-img');
+    pictoClone.setAttribute('pictonr', i);
+    if (pictoC.name) {
+      pictoSource.src = `https://trekjeplan.blob.core.windows.net/pictos/${pictoC.name}`;
     }
-    
-}
+    pictoSource.dataset.img = pictoC.name;
+    pictos.appendChild(pictoClone);
+  }
 
-const showPictos = function(json){
-    for(i in json){
-        let pictoClone = picto.cloneNode(true); 
-        pictoClone.classList.remove('u-hide'); 
-        let pictoC = json[i]
-        let pictoSource = pictoClone.querySelector('.c-choose__picto-img');
-        pictoClone.setAttribute('pictonr', i); 
-        if(pictoC.name){
-        pictoSource.src = `https://trekjeplan.blob.core.windows.net/pictos/${pictoC.name}`; }
-        pictoSource.dataset.img = pictoC.name;
-        pictos.appendChild(pictoClone); 
-        
-    }
+  listenToSelect(json);
+};
 
-    listenToSelect(json);
-}
+const showFilteredPictos = function(json) {
+  console.log('Gefilterde');
+  pictos.innerHTML = '';
+  for (i in json) {
+    let pictoClone = picto.cloneNode(true);
+    pictoClone.classList.remove('u-hide');
+    let pictoC = json[i];
+    let pictoSource = pictoClone.querySelector('.c-choose__picto-img');
+    console.log(pictoSource);
+    pictoClone.setAttribute('pictonr', i);
+    console.log(pictoClone.getAttribute('pictonr'));
+    pictoSource.src = `https://trekjeplan.blob.core.windows.net/pictos/${pictoC.name}`;
+    pictos.appendChild(pictoClone);
+  }
+  listenToSelect(json);
+};
 
-const getPictos = function(url, filtered){
-    fetch(url)
+const getPictos = function(url, filtered) {
+  fetch(url)
     .then(function(response) {
-        if (!response.ok) {
-            throw Error(`Problem to fetch(). Status code: ${response.status}`);
-        } else {
-            return response.json();
+      if (!response.ok) {
+        throw Error(`Problem to fetch(). Status code: ${response.status}`);
+      } else {
+        return response.json();
+      }
+    })
+    .then(function(jsonObject) {
+      let json = jsonObject;
+      if (jsonObject) {
+        if (filtered == true) {
+          showFilteredPictos(json);
+        } else if (filtered == false) {
+          showPictos(json);
         }
+      }
     })
-		.then(function(jsonObject) {
-			let json = jsonObject;
-            if(jsonObject){
-                if(filtered == true){
-                    showFilteredPictos(json);
-                }
-                else if(filtered == false){
-            showPictos(json); 
-        }}
-		})
-		.catch(function(error) {
-			console.log(error);
-			console.error(`Problem to process json ${error}`);
-		});
-}
+    .catch(function(error) {
+      console.log(error);
+      console.error(`Problem to process json ${error}`);
+    });
+};
 
-const listenToSearch = function(){
-    let searchPicto = document.querySelector('.c-input-search'); 
-    searchPicto.addEventListener('input', function(){
-        let url = `${baseURL}picto?search=${searchPicto.value}`; 
-        getPictos(url, true); 
-    })
-}
+const listenToSearch = function() {
+  let searchPicto = document.querySelector('.c-input-search');
+  searchPicto.addEventListener('input', function() {
+    let url = `${baseURL}picto?search=${searchPicto.value}`;
+    getPictos(url, true);
+  });
+};
 
-document.addEventListener('DOMContentLoaded', function(){
-    console.log('DOM loaded - Picto'); 
-    let baseURL ='https://trekjeplan.azurewebsites.net/api/';
-    let url = `${baseURL}picto`;
-    pictos = document.querySelector('.c-choose'); 
-    picto = document.querySelector('.c-choose__picto');
-    init(); 
-    
-    getPictos(url, false); 
-    listenToSearch(); 
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('DOM loaded - Picto');
+  let baseURL = 'https://trekjeplan.azurewebsites.net/api/';
+  let url = `${baseURL}picto`;
+  pictos = document.querySelector('.c-choose');
+  picto = document.querySelector('.c-choose__picto');
+  init();
 
-})
+  getPictos(url, false);
+  listenToSearch();
+});
