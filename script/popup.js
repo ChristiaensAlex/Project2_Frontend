9653d03ea34e9fac19bc2ff44247a1b53308f83f
Alexandra <alexandra.christiaens@student.howest.be>
2020-01-30 05:37:45 +0100

    added delete client and delete progressive scheme function

--- script/popup.js
+++ script/popup.js
@@ -1,4 +1,4 @@
-let deleteClient, background, deleteProgressiveScheme, stepImages, mainImage, divElement;
+let deleteClient, background, deleteProgressiveScheme, stepImages, mainImage, divElement, text;
 
 const OnHandlerClickedCancel = function() {
 	background.classList.remove('c-popup-blur');
@@ -14,24 +14,35 @@ const onHandlerClickedPopUp = function(picto, element) {
 
 	if (document.title == 'Trek Je Plan - Cliënten Overzicht') {
 		document.querySelector('.js-deleteButton').addEventListener('click', function() {
+			text.innerhtml = `${deletedClient.firstName} ${deletedClient.lastName}`;
 			console.log(deletedClient);
 			// hier de delete functie aanspreken voor de client
-			removefromDB('client', deletedClient.id);
+
+			let url = `client/${deletedClient.id}`;
+			removefromDB(url);
 		});
 	} else if (document.title == 'Trek Je Plan - Overzicht stappenplan - Mentor') {
 		document.querySelector('.js-deleteButton').addEventListener('click', function() {
 			console.log(deletedProgressiveScheme);
 			// hier de delete functie aanspreken voor progressive scheme
-			removefromDB('progressiveScheme', deletedProgressiveScheme.id);
+			let url = `progressiveScheme/${deletedProgressiveScheme.id}`;
+			removefromDB(url);
+		});
+	} else if (document.title == 'Trek Je Plan - Overzicht kalender alle cliënten') {
+		document.querySelector('.deleteToday').addEventListener('click', function() {
+			console.log(deletedPlan);
+			console.log(vandaag);
+			// hier de delete functie aanspreken voor progressive scheme
+			let url = `clientprogressiveScheme/${deletedPlan.clientProgressiveSchemeId}/${deletedPlan.clientId}`;
+			removefromDB(url);
 		});
 	}
-
 	if (picto == true) {
 		divElement = element;
 	}
 };
-const removefromDB = function(element, id) {
-	let url = `https://trekjeplan.azurewebsites.net/api/${element}/${id}`;
+const removefromDB = function(endurl) {
+	let url = `https://trekjeplan.azurewebsites.net/api/${endurl}`;
 	fetch(url, {
 		method: 'DELETE'
 	})
@@ -48,6 +59,7 @@ const removefromDB = function(element, id) {
 };
 const ListenToDelete = function(element) {
 	element.addEventListener('click', function() {
+		console.log('klik vb');
 		onHandlerClickedPopUp(false, element);
 	});
 };
@@ -56,15 +68,16 @@ const getElements = function() {
 	deleteClient = document.querySelectorAll('.js-client-delete');
 	background = document.querySelector('.js-background-popup');
 	popup = document.querySelector('.c-popup-form');
+	text = document.querySelector('.js-text');
 	cancelButton = document.querySelector('.js-cancel');
 	if (cancelButton) {
 		cancelButton.addEventListener('click', OnHandlerClickedCancel);
 	}
 	deleteProgressiveScheme = document.querySelectorAll('.js-progressivescheme-delete');
-	deletePlanButton = document.querySelector('.c-planning__delete');
+	//deletePlanButton = document.querySelector('.c-planning__delete');
 	stepImages = document.querySelectorAll('.c-selectedPicto');
 	mainImage = document.querySelector('.c-button__mainStepImage');
-
+	deleteSchedules = document.querySelectorAll('.js-calendar-delete');
 	if (typeof deleteClient != 'undefined' && deleteClient.length > 0) {
 		deleteClient.forEach(element => {
 			ListenToDelete(element);
@@ -73,10 +86,16 @@ const getElements = function() {
 		deleteProgressiveScheme.forEach(element => {
 			ListenToDelete(element);
 		});
-	} else if (deletePlanButton) {
-		console.log(deletePlanButton);
-		ListenToDelete(deletePlanButton);
-	} else if (typeof stepImages != 'undefined' && stepImages.length > 0 && mainImage) {
+	} else if (typeof deleteSchedules != 'undefined' && deleteSchedules.length > 0) {
+		console.log(deleteSchedules);
+		deleteSchedules.forEach(element => {
+			ListenToDelete(element);
+		});
+	} //else if (deletePlanButton) {
+	// 	console.log(deletePlanButton);
+	// 	ListenToDelete(deletePlanButton);
+	// }
+	else if (typeof stepImages != 'undefined' && stepImages.length > 0 && mainImage) {
 		stepImages.forEach(element => {
 			element.addEventListener('click', function() {
 				onHandlerClickedPopUp(true, element);
